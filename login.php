e48198298e8f4a87617a073c3e62a8c22a19d3af
Gekif <dzulfikar.maulana@gmail.com>
2020-03-25 09:24:58 +0700

    4. Coding: 3. 6th class login

--- login.php
+++ login.php
@@ -1,92 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Login V12</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <!--===============================================================================================-->
-</head>
-<body>
+<?php
 
-<div class="limiter">
-    <div class="container-login100" style="background-image: url('images/img-01.jpg');">
-        <div class="wrap-login100 p-t-190 p-b-30">
-            <form class="login100-form validate-form" method="post" action="reg.php">
+require_once 'conn.php';
 
-                <div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
-                    <input class="input100" type="text" name="user" placeholder="Username">
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-							<i class="fa fa-user"></i>
-						</span>
-                </div>
+$user = isset($_POST['user']);
+$pass = isset($_POST['pass']);
 
-                <div class="wrap-input100 validate-input m-b-10" data-validate = "Email is required">
-                    <input class="input100" type="email" name="email" placeholder="Email">
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-							<i class="fa fa-user"></i>
-						</span>
-                </div>
+$sql = "SELECT * FROM membership WHERE 
+        user = '".$user."' AND pass = '".$pass."' LIMIT 1";
 
-                <div class="wrap-input100 validate-input m-b-10" data-validate = "Password is required">
-                    <input class="input100" type="password" name="pass" placeholder="Password">
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-							<i class="fa fa-lock"></i>
-						</span>
-                </div>
+$result = $conn->query($sql);
 
-                <div class="container-login100-form-btn p-t-10">
-                    <button class="login100-form-btn" type="submit" name="submit">
-                        Sign Up
-                    </button>
-                </div>
+if ($result->num_rows > 0) {
+    echo "- You are login";
 
-                <div class="text-center w-full p-t-25 p-b-230">
+    header('Refresh: 2; URL = home.php');
 
-                </div>
-
-                <div class="text-center w-full">
-                    <a class="txt1" href="index.php">
-                        I am already Member
-                        <i class="fa fa-long-arrow-right"></i>
-                    </a>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+} else {
+    echo "- Your email or password is wrong";
+}
 
 
-
-
-<!--===============================================================================================-->
-<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-<script src="vendor/bootstrap/js/popper.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-<script src="js/main.js"></script>
-
-</body>
-</html>
